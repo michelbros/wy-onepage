d711ec66bef41636bbba3689dc63088085791c0e
sunkig <sunk.ig@aol.com>
2019-04-27 12:46:01 +0200

    new assets

--- dist/index.js
+++ dist/index.js
@@ -1,4 +1,5 @@
 const bodyTag = document.querySelector('body');
+const imgTag = document.getElementById('imgId')
 const textTag = document.querySelectorAll('h1');
 const linkTag = document.getElementsByTagName('a');
 
@@ -6,6 +7,7 @@ function changeScene() {
   Array.from(linkTag)
   .forEach(linkTag => linkTag.classList.add('white'));
   bodyTag.classList.add('body');
+  imgTag.src = "assets/softie_wink.gif";
   Array.from(textTag)
   .forEach(textTag => textTag.classList.add('white'));
 }

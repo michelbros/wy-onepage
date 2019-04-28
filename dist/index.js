6c7bbf5facab38d13f0b092fdd5c7e58308a180f
sunkig <sunk.ig@aol.com>
2019-04-28 03:39:30 +0200

    added animation - need to fix loop

--- dist/index.js
+++ dist/index.js
@@ -3,13 +3,24 @@ const imgTag = document.getElementById('imgId')
 const textTag = document.querySelectorAll('h1');
 const linkTag = document.getElementsByTagName('a');
 
-function changeScene() {
+function changeText() {
   Array.from(linkTag)
   .forEach(linkTag => linkTag.classList.add('white'));
-  bodyTag.classList.add('body');
-  imgTag.src = "assets/softie_wink.gif";
   Array.from(textTag)
   .forEach(textTag => textTag.classList.add('white'));
+};
+
+function changeImages() {
+  bodyTag.classList.add('body');
+  imgTag.src = "assets/softie_wink.gif";
+};
+
+function initAnimation() {
+  bodyTag.classList.remove('fadeIn');
+  void bodyTag.offsetWidth;
+  bodyTag.classList.add('fadeIn');
 }
 
-setInterval(changeScene, 3000);
+setInterval(changeText, 3000);
+setInterval(changeImages, 3000);
+setInterval(initAnimation,3001);
